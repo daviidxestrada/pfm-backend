e2e7d2d58c693039f5f215943abb4565699e80d3
daviidxestrada <daviidxestrada@gmail.com>
2026-03-25 07:39:33 +0100

    init: estructura base del backed con Express

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+  res.send('API funcionando');
+});
+
+module.exports = app;
